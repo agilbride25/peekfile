039c884651ad5af4a61a4c7a3ef6aab111de9314
agilbride25 <austinbgilbride@gmail.com>
2025-10-28 17:44:45 +0100

    dynamic line output

--- peek.sh
+++ peek.sh
@@ -1,7 +1,6 @@
 
-working_file="$1";
-head -n 3 "$working_file" > output;
+working_file="$1"; number_of_lines="$2";
+head -n "$number_of_lines" "$working_file" > output;
 echo "..." >> output;
-tail -n 3 "$working_file" >> output;
+tail -n "$number_of_lines" "$working_file" >> output;
 cat output
-
