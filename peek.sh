83e390561bf416e36ce8b57c2c845d8b35aeae05
agilbride25 <austinbgilbride@gmail.com>
2025-11-05 21:15:17 +0100

    added elif

--- peek.sh
+++ peek.sh
@@ -6,6 +6,12 @@ if [[ -n "$number_of_lines" ]]; then
 	echo "..." >> output;
 	tail -n "$number_of_lines" "$working_file" >> output;
 	cat output;
+elif [[ $(wc -l < "$working_file") -le $((2 * number_of_lines)) ]]; then
+	echo "Warning, file has at most double the amount of lines requested";
+	head -n "$number_of_lines" "$working_file" > output;
+	echo "..." >> output;
+	tail -n "$number_of_lines" "$working_file" >> output;
+	cat output;
 else
 	head -n 3 "$working_file" > output;
 	echo "..." >> output;
