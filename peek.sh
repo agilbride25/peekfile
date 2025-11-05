1a9b437181e96b6a3015e9dd64f202ff35236a35
agilbride25 <austinbgilbride@gmail.com>
2025-11-05 20:53:56 +0100

    add if statement

--- peek.sh
+++ peek.sh
@@ -1,6 +1,14 @@
 
 working_file="$1"; number_of_lines="$2";
-head -n "$number_of_lines" "$working_file" > output;
-echo "..." >> output;
-tail -n "$number_of_lines" "$working_file" >> output;
-cat output
+
+if [[ -n "$number_of_lines" ]]; then
+	head -n "$number_of_lines" "$working_file" > output;
+	echo "..." >> output;
+	tail -n "$number_of_lines" "$working_file" >> output;
+	cat output;
+else
+	head -n 3 "$working_file" > output;
+	echo "..." >> output;
+	tail -n 3 "$working_file" >> output;
+	cat output;
+fi
